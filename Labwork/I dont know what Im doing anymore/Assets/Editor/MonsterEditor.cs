16898dd40a68a5f38773581a06a1b4693b8cb338
agentronin316 <svc-student@RC37816.student.directory.jccc>
2015-09-02 09:32:13 -0500

    Started implementation of Lab assignment
    Really need to stop naming projects when I'm not feeling well.

---
+++ Labwork/I dont know what Im doing anymore/Assets/Editor/MonsterEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Monster))]
+public class MonsterEditor : Editor
+{
+    Monster monsterScript;
+    bool visibleRange = false;
+    bool transitionRange = false;
+    bool spawnTypes = false;
+
+    void Awake()
+    {
+        monsterScript = (Monster)target;
+    }
+
+    void OnInspectorGUI()
+    {
+        visibleRange = EditorGUILayout.Foldout(visibleRange, "Visibility Settings");
+
+        transitionRange = EditorGUILayout.Foldout(transitionRange, "Transition Settings");
+
+        spawnTypes = EditorGUILayout.Foldout(spawnTypes, "Spawn Settings");
+
+    }
+}
